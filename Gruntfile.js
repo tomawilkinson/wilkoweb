86b17a0d1f1e7b972dd1ef30e4723aa50964013c
Thomas <thomas@Thomass-MacBook-Pro.local>
2016-04-23 15:30:46 +0100

    Added notify grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,35 +27,65 @@ module.exports = function(grunt) {
         files: {
           'src/assets/js/main.js':
             [
-              'src/assets/js/scripts/function1.js',
-              'src/assets/js/scripts/function2.js'
+              'node_modules/jquery/dist/jquery.js',
+              'src/assets/js/scripts/functions.js'
             ]
         }
-      }
+      },
+      options: {
+        mangle: false,
+        sourceMap : true,
+        sourceMapName : 'src/assets/js/main.js.map'
+      },
     },
     watch: {
       scss: {
         files: ['src/assets/css/scss/**/*.scss'],
-        tasks: ['sass'],
+        tasks: ['sass', 'notify:sass'],
         options: {
           spawn: false,
         },
       },
       js: {
         files: ['src/assets/js/scripts/**/*.js'],
-        tasks: ['uglify'],
+        tasks: ['uglify', 'notify:js'],
         options: {
           spawn: false,
         },
       },
-    }
+    },
 
+    notify: {
+      sass: {
+        options: {
+          title: '<%= pkg.title || pkg.name %> - SCSS',
+          message: 'SCSS Compile finished running',
+        }
+      },
+      js: {
+        options: {
+          title: '<%= pkg.title || pkg.name %> - JS',
+          message: 'JS Uglify finished running',
+        }
+      },
+    }
+    // notify_hooks: {
+
+    //   options: {
+    //     enabled: true,
+    //     max_jshint_notifications: 5, // maximum number of notifications from jshint output
+    //     title: "Project Name", // defaults to the name in package.json, or will use project directory's name
+    //     success: false, // whether successful grunt executions should be notified automatically
+    //     duration: 3 // the duration of notification in seconds, for `notify-send only
+    //   }
+    // }
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-notify');
 
   // Default task.
   //grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
